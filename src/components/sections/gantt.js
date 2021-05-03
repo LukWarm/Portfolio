1c6a0400f53b3b946964cec1a417b3eb0b4beff4
LukWarm <egandonley@gmail.com>
2021-05-03 09:21:48 -0400

    before 1st build

---
+++ src/components/sections/gantt.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledHeroSection = styled.section`
+  ${({ theme }) => theme.mixins.flexCenter};
+  flex-direction: column;
+  align-items: flex-start;
+  min-height: 100vh;
+  padding: 0;
+
+  @media (max-width: 480px) and (min-height: 700px) {
+    padding-bottom: 10vh;
+  }
+
+  h1 {
+    margin: 0 0 30px 4px;
+    color: var(--orange);
+    font-family: var(--font-mono);
+    font-size: clamp(var(--fz-sm), 5vw, var(--fz-md));
+    font-weight: 400;
+
+    @media (max-width: 480px) {
+      margin: 0 0 20px 2px;
+    }
+  }
+
+  h3 {
+    margin-top: 10px;
+    color: var(--slate);
+    line-height: 0.9;
+  }
+
+  p {
+    margin: 20px 0 0;
+    max-width: 500px;
+  }
+
+  .email-link {
+    ${({ theme }) => theme.mixins.bigButton};
+    margin-top: 50px;
+  }
+`;
+
+const Gantt = () => (
+  <StyledHeroSection>
+    <embed
+      src="https://docs.google.com/spreadsheets/d/1wywaEN2EW7OUvb5EXjS0KVWAlHzRo2-P8CRtF-DeB-w/edit?usp=sharing"
+      width="100%"
+      height="800"
+    />
+  </StyledHeroSection>
+);
+export default Gantt;
