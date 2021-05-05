3b05a3cff17a416dde19e950d7f6f506dbdce52c
LukWarm <egandonley@gmail.com>
2021-05-05 02:09:06 -0400

    the reason shit aint working

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 const config = require('./src/config');
 
 module.exports = {
-  pathPrefix: '/portfolio',
+  pathPrefix: '/Portfolio',
   siteMetadata: {
     title: 'Egan Donley',
     description:
