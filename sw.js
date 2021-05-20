f823b29917b910cf3d8ad09f622a266bd1b0c67d
LukWarm <egandonley@gmail.com>
2021-05-20 14:44:57 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-8a95419de9f9d6099e11.js"
+    "url": "webpack-runtime-cb6c1cc0614037f9ca08.js"
   },
   {
     "url": "framework-3321703c33570853116c.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "e554b152544aa3e6fb0b7ba1e904d297"
+    "revision": "68e4870efff52f43cff04ef678c48324"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b1462017a2c34a95e2b4.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "bdfab95dac469b437c03634f942aff86"
+    "revision": "3807c6fd774f9bcc065e774568fc9740"
   },
   {
     "url": "manifest.webmanifest",
